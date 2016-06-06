c2cada629170a32c487168d56b3ee713a779aefd
yokobond <koji.yokokawa@gmail.com>
2016-06-06 13:52:18 +0900

    Move the packaging job to package.json.

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-rm -R ./dist/
-./src/node_modules/.bin/electron-packager ./src "S2E Sound Generator" --platform=darwin --arch=x64 --version=1.2.0 --out=dist --icon=icon/S2E-Sound-Generator-mac.icns --prune
-./src/node_modules/.bin/electron-packager ./src "S2E Sound Generator" --platform=win32 --arch=x64 --version=1.2.0 --out=dist --icon=icon/S2E-Sound-Generator-win.ico --prune
